538391bf0ca37af08cb8b78996d04daba5d60f12
Zenahr <zenmatica@gmail.com>
2021-07-11 19:49:33 +0200

    migrate to SQLalchemy.

--- ZenMatch/app.py
+++ ZenMatch/app.py
@@ -1,35 +1,70 @@
 from flask import Flask, Response, jsonify, request, redirect, url_for
-from model import servers, remove_server_by_id
+from flask_sqlalchemy import SQLAlchemy
+from dataclasses import dataclass # https://stackoverflow.com/a/57732785/12675239
 
 app = Flask(__name__)
-app.config['JSON_SORT_KEYS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite://"
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+app.config['JSON_SORT_KEYS']                 = False
+app.config['DEBUG']                          = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['FLASK_ENV']                      = 'development'
 
+db = SQLAlchemy(app)
+
+@dataclass
+class Server(db.Model): 
+    id              : int
+    name            : str
+    active_players  : int
+    max_players     : int
+    region          : str
+    public_ip       : str
+    description     : str
+
+    id             = db.Column(db.Integer, primary_key=True)
+    name           = db.Column(db.String(60)) # TODO: replace length with config var
+    active_players = db.Column(db.Integer)
+    max_players    = db.Column(db.Integer)
+    region         = db.Column(db.String(60))
+    public_ip      = db.Column(db.String(45))
+    description    = db.Column(db.String(200)) # TODO: replace length with config var
+    # self.is_full
 
 @app.route('/list', methods=['GET'])
 def list_all_servers():
     """list all servers
     """
-    return jsonify(servers)
+    return jsonify(Server.query.all())
 
 @app.route('/add', methods=['GET'])
 def add_new_server():
-    new_server_id  = servers[-1]['id']+1
     name           = request.args.get('name')
     active_players = request.args.get('active_players')
     max_players    = request.args.get('max_players')
     region         = request.args.get('region')
     public_ip      = request.args.get('public_ip')
     description    = request.args.get('description')
-    new_server     = Server(new_server_id, name, active_players, max_players, region, public_ip, description).serialize()
-    servers.append(new_server)
+    new_server     = Server(
+                            name           = name          ,
+                            active_players = active_players,
+                            max_players    = max_players   ,
+                            region         = region        ,
+                            public_ip      = public_ip     ,
+                            description    = description   
+                            )
+    db.session.add(new_server)
+    db.session.commit()
     return jsonify({'message': 'Server has been added to the list.'})
 
 @app.route('/remove', methods=['DELETE']) # TODO: make sure to use UUID to ensure only the owner can remove servers.
 def remove_server():
-    remove_server_by_id(request.args.get('id'))
+    Server.query.filter_by(id=request.args.get('id')).delete()
+    db.session.commit()
     return redirect(url_for('list_all_servers'))
-        
+
+@app.route('/public_ip')
+def get_public_ip():
+    return 'text'
 
 if __name__ == '__main__':
     app.run()
---
+++ ZenMatch/db.py
@@ -0,0 +1,39 @@
+from app import db, Server
+db.create_all()
+
+example_server1 = Server(
+name           = 'example server 1',
+active_players = 2,
+max_players    = 4,
+region         = 'EU',
+public_ip      = '153.97.25.107',
+description    = ''
+)
+
+db.session.add(example_server1)
+db.session.commit()
+
+# def get_servers_count():
+#     count = len(servers)
+#     print(count)
+#     return count
+
+# def get_server_by_id(id):
+#     for server in servers:
+#         if server['id'] == id:
+#             return server
+#         else:
+#             return None
+
+# def get_server_index_by_id(id):
+#     for server in servers:
+#         if server['id'] == id:
+#             return servers.index(server)
+
+# def remove_server_by_id(id):
+#     index = get_server_index_by_id(id)
+#     if (index):
+#         servers.pop(index)
+#         return True # if True then this means the ID has been found and the entry has been removed successfully.
+#     else:
+#         return False
